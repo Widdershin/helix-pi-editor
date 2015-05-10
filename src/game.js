32f58433f93390ff98c7db05901739156301cf53
Nick Johnstone <ncwjohnstone@gmail.com>
2015-05-10 18:51:19 +1200

    Add kiwi js

---
+++ src/game.js
@@ -0,0 +1,25 @@
+
+/**
+* The core HelixPiEditor game file.
+* 
+* This file is only used to initalise (start-up) the main Kiwi Game 
+* and add all of the relevant states to that Game.
+*
+*/
+
+//Initialise the Kiwi Game. 
+
+var gameOptions = {
+	renderer: Kiwi.RENDERER_CANVAS, 
+	width: 800,
+	height: 600
+}
+
+var game = new Kiwi.Game('content', 'HelixPiEditor', null, gameOptions);
+
+//Add all the States we are going to use.
+game.states.addState(HelixPiEditor.Loading);
+game.states.addState(HelixPiEditor.Intro);
+game.states.addState(HelixPiEditor.Play);
+
+game.states.switchState("Loading");
