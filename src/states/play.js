4c432b7f81d123a46375fa71db120e02e4558fea
Nick Johnstone <ncwjohnstone@gmail.com>
2015-05-11 22:15:44 +1200

    Entity is draggable, and frame display

--- src/states/play.js
+++ src/states/play.js
@@ -5,9 +5,32 @@ var HelixPiEditor = HelixPiEditor || {};
 HelixPiEditor.Play = new Kiwi.State('Play');
 
 HelixPiEditor.Play.create = function () {
-  this.entity = new Kiwi.GameObjects.Sprite(this, this.textures.entity);
-  this.entity.y = this.game.stage.height * 0.5 - this.entity.height * 0.5;
-  this.entity.x = this.game.stage.width * 0.5 - this.entity.width * 0.5;
+  this.entity = new Kiwi.GameObjects.Sprite(this, this.textures.entity, 100, 100, true);
   this.addChild(this.entity);
+  this.entity.input.enableDrag();
+
+  this.game.input.keyboard.onKeyDown.add(
+    this.onPress,
+    this
+  );
+
+  this.frameText = new Kiwi.GameObjects.TextField(this, 'test', 10, 10, '#FFF');
+
+  this.addChild(this.frameText);
+
+  this.currentFrame = 0;
 };
 
+HelixPiEditor.Play.update = function () {
+  this.frameText.text = ['Frame: ', this.currentFrame].join('');
+};
+
+HelixPiEditor.Play.onPress = function (keyCode) {
+  if (keyCode === Kiwi.Input.Keycodes.RIGHT) {
+    this.currentFrame += 1;
+  }
+
+  if (keyCode === Kiwi.Input.Keycodes.LEFT) {
+    this.currentFrame -= 1;
+  }
+};
