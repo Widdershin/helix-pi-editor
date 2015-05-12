8100f1638dc7d29cfdb2bda9f8dd645ef82f2268
Nick Johnstone <ncwjohnstone@gmail.com>
2015-05-12 21:56:50 +1200

    Frames now save the entity's position, and added createScenario to create argument for helix-pi

--- src/states/play.js
+++ src/states/play.js
@@ -19,18 +19,67 @@ HelixPiEditor.Play.create = function () {
   this.addChild(this.frameText);
 
   this.currentFrame = 0;
+
+  this.positions = {};
 };
 
 HelixPiEditor.Play.update = function () {
+  Kiwi.State.prototype.update.call(this);
+
   this.frameText.text = ['Frame: ', this.currentFrame].join('');
+};
+
+HelixPiEditor.Play.createScenario = function () {
+  return {
+    startingPosition: function () {
+      return this.positions[0];
+    },
+
+    expectedEndPosition: this.positions[1],
+
+    duration: 60,
+
+    fitness: function (entity) {
+      var distance = {
+        x: Math.abs(this.expectedEndPosition.x - entity.position.x),
+        y: Math.abs(this.expectedEndPosition.y - entity.position.y)
+      };
+
+      return 1000 - (distance.x + distance.y);
+    }
+  };
+};
+
+HelixPiEditor.Play.savePosition = function () {
+  this.positions[this.currentFrame] = {
+    x: this.entity.x,
+    y: this.entity.y
+  };
+};
+
+HelixPiEditor.Play.loadPosition = function () {
+  if (!this.positions[this.currentFrame]) {
+    return;
+  };
+
+  this.entity.x = this.positions[this.currentFrame].x;
+  this.entity.y = this.positions[this.currentFrame].y;
 };
 
 HelixPiEditor.Play.onPress = function (keyCode) {
   if (keyCode === Kiwi.Input.Keycodes.RIGHT) {
+    this.savePosition();
     this.currentFrame += 1;
+    this.loadPosition();
   }
 
   if (keyCode === Kiwi.Input.Keycodes.LEFT) {
+    this.savePosition();
     this.currentFrame -= 1;
+    this.loadPosition();
+  }
+
+  if (keyCode === Kiwi.Input.Keycodes.R) {
+    console.log(this.createScenario());
   }
 };
