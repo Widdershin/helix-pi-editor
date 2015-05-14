86636b9ef21a56ff7039ab254c14292e5475abe6
Nick Johnstone <ncwjohnstone@gmail.com>
2015-05-14 22:49:55 +1200

    Set stuff up for heroku right proper

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/editor/index.html');
 });
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
